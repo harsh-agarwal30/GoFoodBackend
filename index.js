61e5012bffcfca36b4b15f8d7e808d539df05250
harsh-agarwal30 <134685312+harsh-agarwal30@users.noreply.github.com>
2024-07-02 13:24:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORTY || 5000;
+const port = process.env.PORT || 5000;
 
 const mongoDB = require("./db");
 mongoDB(); // Connect to MongoDB
